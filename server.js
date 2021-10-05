349b77fd3891611f2fc182b17b2bd704e3ac5d1c
experiencei <ayelojahighbee01@gmail.com>
2021-10-05 10:00:54 +0100

    using https,manifest and service worker to create PWA

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-var enforce = require('express-sslify');
+var enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
